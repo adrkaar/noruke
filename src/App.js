05033eef97726293823a099b5535902369d944c5
adrkaar <112952665+adrkaar@users.noreply.github.com>
2024-06-16 14:57:02 +0200

    Get some content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import gprofil from './assets/gauteprofil.jpg'
+import { Routes, Route } from "react-router-dom";
+import Home from './routes/home';
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={gprofil} className="App-logo" alt="logo" />
-        <p>
-          Nettside under konstruksjon
-        </p>
-      </header>
-    </div>
+
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={gprofil} className="App-logo" alt="logo" />
+    //     <p>
+    //       Nettside under konstruksjon
+    //     </p>
+    //   </header>
+    // </div>
   );
 }
 
